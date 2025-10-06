8da4baa5981d84c44c120483bb47830056358f2e
Braedyn L <poohdie10@gmail.com>
2025-10-05 21:29:37 -0400

    Add utility functions for BMI and BMR calculations in core/utils.py

---
+++ core/utils.py
@@ -0,0 +1,26 @@
+import math
+from datetime import date
+from io import BytesIO
+
+def calcBmi(weightKg, heightCm):
+    heightMeter = heightCm / 100.0
+    bmi = weightKg / (heightMeter * heightMeter)
+    return round(bmi, 2)
+
+def calcBmiCat(bmi):
+    if bmi <18.5:
+        return "Underweight"
+    elif bmi <25:
+        return "Normal"
+    elif bmi <30:
+        return "Overweight"
+    else:
+        return "Obese"
+
+def calcBmr(weightKg,heightCm,age, sex):
+    if sex == 'male':
+        bmr = 10 * weightKg + 6.25 * heightCm - 5 * age + 5
+    else:
+        bmr = 10 * weightKg + 6.25 * heightCm - 5 * age - 161
+    return round(bmr)
+
