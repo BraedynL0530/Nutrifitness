1b1dee0b448312e32c3fc7b82ef8d32c28e9f3b2
Braedyn L <poohdie10@gmail.com>
2025-10-06 21:48:46 -0400

    Implement user registration, login, and TDEE/protein target calculations

--- core/utils.py
+++ core/utils.py
@@ -24,3 +24,23 @@ def calcBmr(weightKg,heightCm,age, sex):
         bmr = 10 * weightKg + 6.25 * heightCm - 5 * age - 161
     return round(bmr)
 
+lifeStyleFactors = {
+    "Sedentary": 1.2,
+    "Lightly active": 1.375,
+    "Moderately active": 1.55,
+    "Very active": 1.725,
+    "Extremely active": 1.9,
+}
+
+def calcTdee(bmr, activityFactor):
+    return round(bmr * activityFactor)
+
+def proteinTarget(weightkg, goal):
+    if goal == "gain":
+        gPerKg = 1.8
+    elif goal == "lose":
+        gPerKg = 1.2
+    else:
+        gPerKg = 1
+    grams = round(weightkg * gPerKg)
+    return grams
--- core/views.py
+++ core/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
 # Create your views here.
-from django.shortcuts import render
-
 def home(request):
     return render(request, 'home.html')
+
+def register(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # auto-login
+            return redirect("home")
+    else:
+        form = UserCreationForm()
+    return render(request, "register.html", {"form": form})
